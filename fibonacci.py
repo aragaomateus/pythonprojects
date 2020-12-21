64d8921662ba5b775d8441492d70bc0d4a81215f
MateusSilvaAragao <aragaosm@icloud.com>
2020-12-21 18:07:27 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,10 @@ for i in range(99):
     else:
         numbers.insert(i, numbers[i-1] + numbers[i-2])
 print (numbers)
+
+# Sample visualization. 
+for i in range(13):
+    for x in range(numbers[i]):
+        print(".", end="")
+    print()
+print(numbers[len(numbers)-3])
